f2fb59999fb23864e050d2d6187e3b5b65b02da9
Angel Axel Gonzalez <utm25090262@utma.edu.mx>
2026-03-22 21:44:55 +0000

    ya subete readme

--- target/debug/deps/proyecto_eli-c4940854a8879326.d
+++
@@ -1,5 +0,0 @@
-/workspaces/Proyecto_eli_v2/target/debug/deps/proyecto_eli-c4940854a8879326.d: src/main.rs
-
-/workspaces/Proyecto_eli_v2/target/debug/deps/proyecto_eli-c4940854a8879326: src/main.rs
-
-src/main.rs:
--- target/debug/proyecto_eli.d
+++
@@ -1,1 +0,0 @@
-/workspaces/Proyecto_eli_v2/target/debug/proyecto_eli: /workspaces/Proyecto_eli_v2/src/main.rs
